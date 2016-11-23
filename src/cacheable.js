b33fc8a9fa073ab7a5dae2318d32b9185fa2dbd4
Churchill Lee <churchill@piktochart.com>
2016-11-23 11:34:52 +0800

    refactor(cacheable): remove semicolons

--- src/cacheable.js
+++ src/cacheable.js
@@ -2,14 +2,14 @@ import xs from 'xstream';
 
 export function Cacheable( Component ) {
   return function( sources ) {
-    const cache$ = xs.createWithMemory();
+    const cache$ = xs.createWithMemory()
 
-    const sinks = Component( Object.assign({}, sources, {cache: cache$ }) );
+    const sinks = Component( Object.assign({}, sources, {cache: cache$ }) )
 
     const cacheReducer$ = sinks.cache
     .map( state => function updateCache( cache ) {
       if ( ! state.name ) {
-        console.warn( 'State has no name attribute. Not caching.' );
+        console.warn( 'State has no name attribute. Not caching.' )
         return cache;
       }
 
@@ -17,13 +17,12 @@ export function Cacheable( Component ) {
     });
 
     const proxyCache$ = cacheReducer$.fold((cache, reducer) => reducer( cache ), {})
-    .debug( 'cache' );
 
     // cache$.imitate( proxyCache$ );
     proxyCache$.addListener({
       next: ( value ) => cache$.shamefullySendNext( value )
-    });
+    })
 
-    return sinks;
+    return sinks
   }
 }
